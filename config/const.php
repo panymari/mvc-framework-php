3d868a0bd04b57732e202c0999b01ade25a2a770
Maria Pashkovskaya <panymari84@gmail.com>
2022-07-07 14:51:43 +0300

    Bugfix

--- config/const.php
+++ config/const.php
@@ -6,7 +6,6 @@ const CONFIG_ROOT = ROOT . '/config/'; // configuration root folder
 const UPLOAD_FOLDER = ROOT . '/upload'; //upload folder
 const LOGS_FOLDER = ROOT . '/logs'; //upload folder
 
-
 //ref
 define('ROOT_REF', 'http://' . $_SERVER['HTTP_HOST']); // root ref
 define('ROOT_REF_FILE', 'http://' . $_SERVER['HTTP_HOST'] . '/file'); // ref of the view
