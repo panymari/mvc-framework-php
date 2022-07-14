75138825e3bb098d2b41b5d16df8c09dc5b872c9
Maria Pashkovskaya <panymari84@gmail.com>
2022-07-14 11:52:42 +0300

    Delete unnecessary file

--- reg-form/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'user/signup' => 'user/signup',
-    'user/login' => 'user/login',
-    'user/logout' => 'user/logout',
-    'user/profile' => 'user/profile',
-    'user' => 'user/index',
-];
